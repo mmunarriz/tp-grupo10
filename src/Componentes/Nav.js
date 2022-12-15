c8c8036309405364ef184263508e1d29556ff69f
Mariano Munarriz <mmunarriz@gmail.com>
2022-12-15 09:21:41 -0300

    fix more errors

--- src/Componentes/Nav.js
+++ src/Componentes/Nav.js
@@ -16,15 +16,41 @@ function Nav() {
   }, []);
 
   return (
+
     <div className={`nav ${show && "nav_blank"}`}>
-      <img
-        className="nav_logo"
-        src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
-        alt="Netflix Logo"
-      />
-      <a href="Profile">
-        <img className="nav__avatar" src="images/avatar-7.png" alt="avatar" />
-      </a>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <img
+                className="nav_logo"
+                src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+                alt="Netflix Logo"
+              />
+            </a>
+          </li>
+
+          <li className="cont-user">
+            <a className="nav__avatar">
+              <i className="fa-solid fa-user"></i>
+            </a>
+            <hr />
+            <ul className="links">
+              <div className="triangulo"></div>
+              <li id="primer-li">
+                <a href="/mostrarfavoritas">Mis Favoritos</a>
+              </li>
+              <hr />
+              <li>
+                <a href="/logout">Log Out</a>
+              </li>
+              <li>
+                <a href="#">Profile</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
--- src/Componentes/TopRated.js
+++ src/Componentes/TopRated.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import MovieCard from "./MovieCard";
-import { useAuth } from "../context/authContext";
+
 
 function TopRated() {
-  const { user } = useAuth();
-  console.log(user.email)
   const [movies, setMovies] = useState([]);
   let [page, setPage] = useState(1);
 
